51a2947fb5c62fc4b5ce443e1a81447d866dcd9b
vuquangdang2002 <vuquangdang2002@gmail.com>
2026-05-17 19:57:29 +0700

    fix: change domain to vu-family to comply with dns rules

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,6 @@
 // Application Configuration
 
 // Tên miền chính của hệ thống (sửa tại đây sẽ tự động cập nhật API và Firebase)
-export const APP_DOMAIN = 'vu_family.dangvq.online';
+export const APP_DOMAIN = 'vu-family.dangvq.online';
 
 export const API_BASE_URL = `https://${APP_DOMAIN}/api`;
