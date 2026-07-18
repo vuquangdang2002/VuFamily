85fc96ed465cfd94a005d8450c29cfe1c48f1a60
vuquangdang2002 <vuquangdang2002@gmail.com>
2026-07-18 13:19:00 +0700

    fix: inject require shim using esbuild banner to resolve dynamic require of http

--- scripts/build-worker.js
+++ scripts/build-worker.js
@@ -13,6 +13,65 @@ const mockIconvPlugin = {
   }
 };
 
+const bannerJs = `
+import nodeHttp from 'node:http';
+import nodePath from 'node:path';
+import nodeCrypto from 'node:crypto';
+import nodeEvents from 'node:events';
+import nodeUtil from 'node:util';
+import nodeStream from 'node:stream';
+import nodeZlib from 'node:zlib';
+import nodeUrl from 'node:url';
+import nodeQuerystring from 'node:querystring';
+import nodeTimers from 'node:timers';
+import nodeBuffer from 'node:buffer';
+import nodeStringDecoder from 'node:string_decoder';
+import nodeNet from 'node:net';
+import nodeTls from 'node:tls';
+import nodeAssert from 'node:assert';
+import nodeOs from 'node:os';
+
+const shimModules = {
+  'http': nodeHttp,
+  'path': nodePath,
+  'crypto': nodeCrypto,
+  'events': nodeEvents,
+  'util': nodeUtil,
+  'stream': nodeStream,
+  'zlib': nodeZlib,
+  'url': nodeUrl,
+  'querystring': nodeQuerystring,
+  'timers': nodeTimers,
+  'buffer': nodeBuffer,
+  'string_decoder': nodeStringDecoder,
+  'net': nodeNet,
+  'tls': nodeTls,
+  'assert': nodeAssert,
+  'os': nodeOs,
+  'node:http': nodeHttp,
+  'node:path': nodePath,
+  'node:crypto': nodeCrypto,
+  'node:events': nodeEvents,
+  'node:util': nodeUtil,
+  'node:stream': nodeStream,
+  'node:zlib': nodeZlib,
+  'node:url': nodeUrl,
+  'node:querystring': nodeQuerystring,
+  'node:timers': nodeTimers,
+  'node:buffer': nodeBuffer,
+  'node:string_decoder': nodeStringDecoder,
+  'node:net': nodeNet,
+  'node:tls': nodeTls,
+  'node:assert': nodeAssert,
+  'node:os': nodeOs
+};
+
+const require = (id) => {
+  if (shimModules[id]) return shimModules[id];
+  throw new Error("Dynamic require of " + id + " is not supported");
+};
+`;
+
 esbuild.build({
   entryPoints: [path.resolve(__dirname, '../worker.js')],
   bundle: true,
@@ -59,6 +118,9 @@ esbuild.build({
     'node:os'
   ], 
   plugins: [mockIconvPlugin],
+  banner: {
+    js: bannerJs
+  },
   minify: false,
   sourcemap: true,
 }).then(() => {
